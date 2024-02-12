2f0ea1eb073cad2457a3da7284cca923eb976cb1
Przemysław Żydek <przemyslawzydek@gmail.com>
2024-02-12 16:33:17 +0800

    chore: use shared config for prettier and eslint

--- src/middlewares/beforeResponse.middleware.ts
+++ src/middlewares/beforeResponse.middleware.ts
@@ -1,18 +1,18 @@
-import { ApplicationLogger, RequestPayload, ResponsePayload } from '../types';
-import { NextFunction, Request, Response } from 'express';
+import { ApplicationLogger, RequestPayload, ResponsePayload } from '../types'
+import { NextFunction, Request, Response } from 'express'
 
 const beforeResponseMiddleware = (logger: ApplicationLogger) => (req: Request, res: Response, next: NextFunction) => {
-  let requestResolve: (payload: RequestPayload) => void;
-  let responseResolve: (payload: ResponsePayload) => void;
-  let requestReject, responseReject;
+  let requestResolve: (payload: RequestPayload) => void
+  let responseResolve: (payload: ResponsePayload) => void
+  let requestReject, responseReject
   const requestPromise = new Promise<RequestPayload>((reqResolve, reqReject) => {
-    requestResolve = reqResolve;
-    requestReject = reqReject;
-  });
+    requestResolve = reqResolve
+    requestReject = reqReject
+  })
   const responsePromise = new Promise<ResponsePayload>((resResolve, resReject) => {
-    responseResolve = resResolve;
-    responseReject = resReject;
-  });
+    responseResolve = resResolve
+    responseReject = resReject
+  })
 
   req.on('end', () => {
     requestResolve({
@@ -21,29 +21,29 @@ const beforeResponseMiddleware = (logger: ApplicationLogger) => (req: Request, r
       method: req.method,
       queryParams: req.query as unknown as { [key: string]: string },
       urlHost: req.get('host'),
-    });
-  });
+    })
+  })
   req.on('error', (err) => {
-    requestReject(err);
-  });
+    requestReject(err)
+  })
 
   res.on('finish', () => {
     responseResolve({
       contentType: res.getHeaders().host,
       statusCode: res.statusCode,
-    });
-  });
+    })
+  })
   res.on('error', (err) => {
-    responseReject(err);
-  });
-  next();
+    responseReject(err)
+  })
+  next()
 
   Promise.all([requestPromise, responsePromise]).then(([request, response]) => {
     logger({
       requestPayload: request,
       responsePayload: response,
-    });
-  });
-};
+    })
+  })
+}
 
-export default beforeResponseMiddleware;
+export default beforeResponseMiddleware
--- src/types.ts
+++ src/types.ts
@@ -1,18 +1,18 @@
 export interface RequestPayload {
-  urlHost: string;
-  path: string;
-  queryParams: { [key: string]: string };
-  headers: { [key: string]: string };
-  method: string;
+  urlHost: string
+  path: string
+  queryParams: { [key: string]: string }
+  headers: { [key: string]: string }
+  method: string
 }
 
 export interface ResponsePayload {
-  contentType: string;
-  statusCode: number;
+  contentType: string
+  statusCode: number
 }
 export interface ApplicationLog {
-  requestPayload: RequestPayload;
-  responsePayload: ResponsePayload;
+  requestPayload: RequestPayload
+  responsePayload: ResponsePayload
 }
 
-export type ApplicationLogger = (log: ApplicationLog) => void;
+export type ApplicationLogger = (log: ApplicationLog) => void
