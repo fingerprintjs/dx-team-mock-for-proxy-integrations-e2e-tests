0c834116ef63d27ae818778d5cd14346007b35bc
Przemysław Żydek <przemyslawzydek@gmail.com>
2024-02-13 17:48:59 +0800

    style: format

--- src/app/test/service/testCases.ts
+++ src/app/test/service/testCases.ts
@@ -1,44 +1,44 @@
-import { Request as ExpressRequest, Response as ExpressResponse } from 'express';
-import { TestCaseApi } from './TestCaseApi';
+import { Request as ExpressRequest, Response as ExpressResponse } from 'express'
+import { TestCaseApi } from './TestCaseApi'
 
 export type SendRequestResult = {
-  requestFromProxy: ExpressRequest;
+  requestFromProxy: ExpressRequest
   // TODO: Not sure if this will be needed
-  sendResponse: (response: ExpressResponse) => void;
-};
+  sendResponse: (response: ExpressResponse) => void
+}
 
 export type FailedTestResult = {
-  passed: false;
-  reason: string;
-};
+  passed: false
+  reason: string
+}
 
 export type PassedTestResult = {
-  passed: true;
-};
+  passed: true
+}
 
-export type TestResult = FailedTestResult | PassedTestResult;
+export type TestResult = FailedTestResult | PassedTestResult
 
 export type TestCase = {
-  name: string;
-  test: (api: TestCaseApi) => Promise<void>;
-};
+  name: string
+  test: (api: TestCaseApi) => Promise<void>
+}
 
 export const testCases: TestCase[] = [
   {
     name: 'agent request query params',
     test: async (api) => {
-      const query = new URLSearchParams();
-      query.set('apiKey', 'test');
-      query.set('version', '3');
-      query.set('loaderVersion', '3.6.5');
+      const query = new URLSearchParams()
+      query.set('apiKey', 'test')
+      query.set('version', '3')
+      query.set('loaderVersion', '3.6.5')
 
-      const { requestFromProxy } = await api.sendRequestToCdn(query);
+      const { requestFromProxy } = await api.sendRequestToCdn(query)
 
-      const params = requestFromProxy.params as Record<string, string | undefined>;
+      const params = requestFromProxy.params as Record<string, string | undefined>
 
-      api.assert(params.apiKey, query.get('apiKey'));
-      api.assert(params.version, 'v3');
-      api.assert(params.loader, 'loader_v3.6.5.js');
+      api.assert(params.apiKey, query.get('apiKey'))
+      api.assert(params.version, 'v3')
+      api.assert(params.loader, 'loader_v3.6.5.js')
     },
   },
-];
+]
