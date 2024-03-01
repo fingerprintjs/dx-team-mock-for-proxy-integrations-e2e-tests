0e2edb647c0b44299bef3d1d5808f533f4a4569d
Przemysław Żydek <przemyslawzydek@gmail.com>
2024-03-01 17:45:37 +0800

    build: update test runner build

--- test-runner/dist/main.js
+++ test-runner/dist/main.js
@@ -5143,14 +5143,14 @@ function retry(policy, opts) {
 }
 
 const logger = createConsola();
-const API_URL = process.env.API_URL ?? 'https://mock-warden.fpjs.sh';
 const args = argumentParser({
     options: RunTestsRequestSchema.extend({
         attempts: z.number().default(3),
+        apiUrl: z.string().url(),
     }).strict(),
 }).parse(process.argv.slice(2));
 async function main() {
-    const url = new URL(API_URL);
+    const url = new URL(args.apiUrl);
     url.pathname = '/api/test/run-tests';
     logger.info(`Using ${url} as API url`);
     logger.start(`Sending request...`);
