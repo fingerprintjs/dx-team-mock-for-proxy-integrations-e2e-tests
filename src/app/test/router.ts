244416c697b5efcca610cda41ce3657585056c39
Orkun <orkuncakilkaya@gmail.com>
2024-04-22 13:10:15 +0300

    chore: finalize test session if it fails

--- src/app/test/router.ts
+++ src/app/test/router.ts
@@ -1,6 +1,6 @@
 import * as express from 'express'
 import { RunTestsRequestSchema } from './request.types'
-import { createTestSession } from './service/session'
+import { createTestSession, finalizeTestSession } from './service/session'
 import { runTests } from './service/testRunner'
 import { validateRequest } from 'zod-express-middleware'
 
@@ -12,13 +12,13 @@ export function testRouter() {
   const router = express.Router()
 
   router.post('/run-tests', RunTestsSchema, async (req, res, next) => {
+    const testSession = createTestSession(req.body)
     try {
-      const testSession = createTestSession(req.body)
-
       const result = await runTests(testSession)
 
       return res.json(result.toTestResponse())
     } catch (e) {
+      finalizeTestSession(testSession)
       next(e)
     }
   })
