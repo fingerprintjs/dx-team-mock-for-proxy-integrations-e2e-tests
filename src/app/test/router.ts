1f51b487d655fbb967d6780132b371756571cc12
Orkun <orkuncakilkaya@gmail.com>
2024-04-23 11:03:51 +0300

    fix: test session initializing

--- src/app/test/router.ts
+++ src/app/test/router.ts
@@ -1,6 +1,6 @@
 import * as express from 'express'
 import { RunTestsRequestSchema } from './request.types'
-import { createTestSession, finalizeTestSession } from './service/session'
+import { createTestSession, finalizeTestSession, TestSession } from './service/session'
 import { runTests } from './service/testRunner'
 import { validateRequest } from 'zod-express-middleware'
 
@@ -12,13 +12,16 @@ export function testRouter() {
   const router = express.Router()
 
   router.post('/run-tests', RunTestsSchema, async (req, res, next) => {
-    const testSession = createTestSession(req.body)
+    let testSession: TestSession
     try {
+      testSession = createTestSession(req.body)
       const result = await runTests(testSession)
 
       return res.json(result.toTestResponse())
     } catch (e) {
-      finalizeTestSession(testSession)
+      if (testSession) {
+        finalizeTestSession(testSession)
+      }
       next(e)
     }
   })
