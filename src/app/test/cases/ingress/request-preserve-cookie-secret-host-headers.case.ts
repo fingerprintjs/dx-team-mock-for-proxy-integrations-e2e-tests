bd08648c990b32136d7bcdfbe59042607e488115
Przemysław Żydek <przemyslawzydek@gmail.com>
2024-04-23 18:16:55 +0900

    fix: remove duplicate ip check

--- src/app/test/cases/ingress/request-preserve-cookie-secret-host-headers.case.ts
+++ src/app/test/cases/ingress/request-preserve-cookie-secret-host-headers.case.ts
@@ -1,6 +1,5 @@
-import { assert, assertEqualIp } from '../../service/assert'
+import { assert } from '../../service/assert'
 import { TestCase } from '../../types/testCase'
-import { getIP } from '../../utils/getIP'
 
 const testCase: TestCase = {
   name: 'ingress request with cookie filter and preservation of headers',
@@ -12,10 +11,7 @@ const testCase: TestCase = {
       },
     })
 
-    const ipOfClient = await getIP()
-
     assert(requestFromProxy.get('x-custom-header'), '123', 'x-custom-header')
-    assertEqualIp(requestFromProxy.get('fpjs-proxy-client-ip'), ipOfClient, 'fpjs-proxy-client-ip')
     assert(requestFromProxy.get('cookie'), '_iidt=123', 'cookie')
   },
 }
