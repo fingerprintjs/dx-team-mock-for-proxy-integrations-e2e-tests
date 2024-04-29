41ed7cccaf9fa3fa734c3f035128a13f2cccc12c
Orkun <orkuncakilkaya@gmail.com>
2024-04-29 12:12:35 +0300

    fix: falsy values with assert to be falsy

--- src/app/test/service/assert.ts
+++ src/app/test/service/assert.ts
@@ -47,12 +47,36 @@ export function assertRegExp(actual: string, regExp: RegExp, message?: string) {
 }
 
 export function assertToBeTruthy(field: string, actual: any, message?: string) {
+  const safeActual = typeof actual === 'string' ? actual.trim() : actual
   if (!actual) {
-    throw new AssertionError({
-      operator: 'assertToBeTruthy',
-      actual,
-      expected: field,
-      message,
-    })
+    return
+  } else if ((Array.isArray(actual) || typeof actual === 'string') && safeActual.length > 0) {
+    return
   }
+
+  throw new AssertionError({
+    operator: 'assertToBeTruthy',
+    actual,
+    expected: field,
+    message,
+  })
+}
+
+export function assertToBeFalsy(field: string, actual: any, message?: string) {
+  if (!actual) {
+    return
+  }
+
+  const safeActual = typeof actual === 'string' ? actual.trim() : actual
+
+  if ((Array.isArray(actual) || typeof actual === 'string') && safeActual.length === 0) {
+    return
+  }
+
+  throw new AssertionError({
+    operator: 'assertToBeFalsy',
+    actual,
+    expected: field,
+    message,
+  })
 }
