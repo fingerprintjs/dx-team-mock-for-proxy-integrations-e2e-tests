62b2b18a32e0489e06dc742e015212d50c4cae20
Przemysław Żydek <przemyslawzydek@gmail.com>
2024-07-08 13:51:04 +0200

    chore: use random strings

--- src/app/test/cases/browser-cache/browser-cache-preserve-path.case.ts
+++ src/app/test/cases/browser-cache/browser-cache-preserve-path.case.ts
@@ -1,10 +1,11 @@
 import { assert } from '../../service/assert'
 import { TestCase } from '../../types/testCase'
+import { getRandomString } from '../../utils/getRandomString'
 
 const testCase: TestCase = {
   name: 'browser cache preserve path',
   test: async (api) => {
-    const path = '/test/nested/path'
+    const path = `/${getRandomString()}/${getRandomString()}/${getRandomString()}`
     const { requestFromProxy } = await api.sendRequestToCacheEndpoint({}, undefined, path)
 
     const requestUrl = new URL(`https://${requestFromProxy.get('host')}${requestFromProxy.url}`)
