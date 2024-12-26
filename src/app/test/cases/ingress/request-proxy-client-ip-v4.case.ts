3775491a0bdc85b6f7f18595e954ef64b216efcc
Orkun <orkuncakilkaya@gmail.com>
2024-12-26 13:39:31 +0300

    feat: add ipv6 and ipv4 testing capabilities

---
+++ src/app/test/cases/ingress/request-proxy-client-ip-v4.case.ts
@@ -0,0 +1,31 @@
+import axios from 'axios'
+import * as dns from 'node:dns'
+import { assertEqualIp } from '../../service/assert'
+import { TestCase } from '../../types/testCase'
+import { getIPv4 } from '../../utils/getIPv4'
+
+const testCase: TestCase = {
+  name: 'ingress request get proxy client ipv4 and validity',
+  before: () => {
+    axios.defaults.lookup = (hostname, _, cb) => {
+      dns.lookup(hostname, 4, (_, addresses) => {
+        cb(null, { address: addresses, family: 4 })
+      })
+    }
+  },
+  test: async (api) => {
+    const { requestFromProxy } = await api.sendRequestToIngress({
+      headers: {
+        host: api.testSession.host,
+      },
+    })
+    const ipOfClient = await getIPv4()
+
+    assertEqualIp(requestFromProxy.get('fpjs-proxy-client-ip'), ipOfClient, 'fpjs-proxy-client-ip')
+  },
+  after: () => {
+    axios.defaults.lookup = undefined
+  },
+}
+
+export default testCase
--- src/app/test/utils/getIP.ts
+++
@@ -1,6 +0,0 @@
-import { httpClient } from '../../../utils/httpClient'
-
-const IP_ADDRESS_PROVIDER = 'https://checkip.amazonaws.com'
-export const getIP = async () => {
-  return (await httpClient.get<string>(IP_ADDRESS_PROVIDER).then((res) => res.data)).trim()
-}
