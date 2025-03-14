f0b08d4386d9074f3990f42d33a75104cfb1d855
Orkun <orkuncakilkaya@gmail.com>
2025-03-14 10:58:06 +0300

    feat: get ipv4 address of instance from another external service

--- src/app/test/utils/getIP.ts
+++ src/app/test/utils/getIP.ts
@@ -1,17 +1,12 @@
 import { httpClient } from '../../../utils/httpClient'
 
-const IPv4_ADDRESS_PROVIDER = 'http://169.254.169.254/latest/meta-data/public-ipv4' // Local IP address of Amazon Public Services
+const IPv4_ADDRESS_PROVIDER = 'https://ifconfig.me' // Local IP address of Amazon Public Services
 const IPv6_ADDRESS_PROVIDER = 'http://169.254.169.254/latest/meta-data/ipv6' // Local IP address of Amazon Public Services
 const IMDS_TOKEN_PROVIDER = 'http://169.254.169.254/latest/api/token' // The endpoint for returns the IMDS token required for authentication
 // See here for more information: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instancedata-data-retrieval.html
 
 export const getIPv4 = async () => {
-  const token = await getTokenForIMDS()
-  return (
-    await httpClient
-      .get<string>(IPv4_ADDRESS_PROVIDER, { headers: { 'X-aws-ec2-metadata-token': token } })
-      .then((res) => res.data)
-  ).trim()
+  return (await httpClient.get<string>(IPv4_ADDRESS_PROVIDER).then((res) => res.data)).trim()
 }
 
 export const getIPv6 = async () => {
