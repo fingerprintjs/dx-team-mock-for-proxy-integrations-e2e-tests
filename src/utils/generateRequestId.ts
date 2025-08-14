ce7917689aed7237bdda8bea18daa46681eff05c
Eray Aydın <eray.aydin@fingerprint.com>
2025-08-14 17:47:57 +0300

    fix: import crypto for generateRequestId

--- src/utils/generateRequestId.ts
+++ src/utils/generateRequestId.ts
@@ -1,7 +1,9 @@
+import { webcrypto } from 'node:crypto'
+
 export function generateRequestId(): string {
   const timestamp = Math.floor(Date.now() / 1000)
   const array = new Uint8Array(3)
-  crypto.getRandomValues(array)
+  webcrypto.getRandomValues(array)
   const randomString = Array.from(array, (b) => b.toString(36).padStart(2, '0')).join('')
 
   return `${timestamp}.${randomString}`
