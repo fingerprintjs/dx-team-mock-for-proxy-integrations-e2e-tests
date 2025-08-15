3a95b5e4c74e37c4214ae9f64e7c570b960fa255
Eray Aydın <eray.aydin@fingerprint.com>
2025-08-15 22:33:32 +0300

    feat(test-runner): add new parameters, auto-derive
    Extended CLI schema to accept `integrationUrl`, `ingressPath`, and
    `cdnPath`. Added `parsePaths()` to resolve these values for old
    parameters. If `integrationUrl` used, required both `ingressPath` and
    `cdnPath` values. If there is no `integrationUrl` parameter, validated
    (origin) and used `ingressProxyUrl` and `cdnProxyUrl` to determine
    `integrationUrl` (common part of the URLs), `ingressPath`, `cdnPath`.
    Added deprecation warning for old parameter usage.
    
    Notes: This is a non-breaking change. Old parameters still work but are
    deprecated.
    
    Task-Id: INTER-1390

--- test-runner/src/main.ts
+++ test-runner/src/main.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { argumentParser } from 'zodcli'
-import { RunTestsRequestSchema } from '../../src/app/test/request.types'
+import { RunTestsRequest, RunTestsRequestSchema } from '../../src/app/test/request.types'
 import { TestResponse } from '../../src/app/test/service/session'
 import { createConsola } from 'consola'
 import { DetailedTestResult } from '../../src/app/test/service/testRunner'
@@ -16,12 +16,95 @@ const args = argumentParser({
   options: RunTestsRequestSchema.extend({
     attempts: z.number().default(3),
     apiUrl: z.string().url(),
+    integrationUrl: z.string().url().optional(),
+    ingressPath: z.string().optional(),
+    cdnPath: z.string().optional(),
+    cdnProxyUrl: z.string().url().optional(),
+    ingressProxyUrl: z.string().url().optional(),
+    trafficName: z.string(),
+    integrationVersion: z.string(),
+    testsFilter: z.array(z.string()).optional(),
   }).strict(),
 }).parse(process.argv.slice(2))
+
+function parsePaths() {
+  if (args.integrationUrl) {
+    if (!args.ingressPath || !args.cdnPath) {
+      throw new Error("Missing required params: provide 'ingressPath' and 'cdnPath' when using 'integrationUrl'.")
+    }
+
+    return {
+      integrationUrl: args.integrationUrl,
+      ingressPath: args.ingressPath,
+      cdnPath: args.cdnPath,
+    }
+  }
+
+  if (!args.cdnProxyUrl || !args.ingressProxyUrl) {
+    throw new Error(
+      "Missing required params: provide 'ingressProxyUrl' and 'cdnProxyUrl' when not using 'integrationUrl'."
+    )
+  }
+
+  const cdnProxyUrl = new URL(args.cdnProxyUrl)
+  const ingressProxyUrl = new URL(args.ingressProxyUrl)
+
+  if (cdnProxyUrl.origin !== ingressProxyUrl.origin) {
+    throw new Error("Invalid arguments: 'cdnProxyUrl' and 'ingressProxyUrl' must share the same origin.")
+  }
+
+  const splitParts = (p: string) =>
+    p
+      .replace(/\/+/g, '/') // if there are multiple `/` character, makes them single
+      .replace(/^\/|\/$/g, '') // if there are starting `/` character or ending `/` character, remove it
+      .split('/')
+      .filter(Boolean)
+
+  const findCommonPrefix = (a: string[], b: string[]) => {
+    const res: string[] = []
+    for (let i = 0; i < Math.min(a.length, b.length); i++) {
+      if (a[i] !== b[i]) {
+        break
+      }
+      res.push(a[i])
+    }
+    return res
+  }
+
+  const cdnSegments = splitParts(cdnProxyUrl.pathname)
+  const ingressSegments = splitParts(ingressProxyUrl.pathname)
+  const commonPathSegments = findCommonPrefix(cdnSegments, ingressSegments)
+
+  const integrationPath = commonPathSegments.length ? `/${commonPathSegments.join('/')}` : ''
+  const integrationUrl = cdnProxyUrl.origin + integrationPath
+
+  const findSubPath = (full: string[], length: number) => full.slice(length).join('/')
+
+  const cdnPath = findSubPath(cdnSegments, commonPathSegments.length) || '/'
+  const ingressPath = findSubPath(ingressSegments, commonPathSegments.length) || '/'
+
+  return {
+    integrationUrl,
+    cdnPath,
+    ingressPath,
+  }
+}
 
 async function main() {
   const url = new URL(args.apiUrl)
   url.pathname = '/api/test/run-tests'
+
+  if (args.ingressProxyUrl || args.cdnProxyUrl) {
+    logger.box(
+      '[DEPRECATION] ingressProxyUrl and cdnProxyUrl will be removed in a future release. Use integration, ingress-path, and cdn-path parameters.'
+    )
+  }
+
+  const { integrationUrl, cdnPath, ingressPath } = parsePaths()
+
+  logger.info(`Found integrationUrl: ${integrationUrl}`)
+  logger.info(`Found cdnPath: ${cdnPath}`)
+  logger.info(`Found ingressPath: ${ingressPath}`)
 
   logger.info(`Using ${url} as API url`)
   logger.start(`Sending request...`)
@@ -34,7 +117,16 @@ async function main() {
   await policy.execute(async (context) => {
     logger.info(`Attempt ${context.attempt}...`)
 
-    const response = await httpClient.post<TestResponse>(url.toString(), JSON.stringify(args), {
+    const requestBody = {
+      integrationUrl,
+      ingressPath,
+      cdnPath,
+      trafficName: args.trafficName,
+      integrationVersion: args.integrationVersion,
+      testsFilter: args.testsFilter,
+    } satisfies RunTestsRequest
+
+    const response = await httpClient.post<TestResponse>(url.toString(), JSON.stringify(requestBody), {
       headers: {
         'content-type': 'application/json',
       },
