f9d3bae6dffc626999383ec352d3e258a64d129b
Eray Aydın <eray.aydin@fingerprint.com>
2025-08-20 15:54:06 +0300

    build: enable JSON imports in Rollup
    Add `@rollup/plugin-json` to `test-runner` subproject and add the plugin
    in `rollup.config.mjs` file.

--- test-runner/rollup.config.mjs
+++ test-runner/rollup.config.mjs
@@ -1,6 +1,7 @@
 import typescript from 'rollup-plugin-typescript2';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
 
 const outputDirectory = 'dist';
 
@@ -16,6 +17,7 @@ function makeConfig(entryFile, artifactName) {
       }),
       nodeResolve(),
       commonjs(),
+      json(),
     ],
   };
 
