8f773f2fcbae24d3f501d4b87677c84d88ee158b
Przemysław Żydek <przemyslawzydek@gmail.com>
2026-03-17 10:48:25 +0100

    chore: revert cookie test case changes

--- src/app/test/cases/agent/request-v4-no-cookie.case.ts
+++ src/app/test/cases/agent/request-v4-no-cookie.case.ts
@@ -1,15 +1,15 @@
-import { assert } from '../../service/assert'
+import { assertToBeFalsy } from '../../service/assert'
 import { TestCase } from '../../types/testCase'
 
 const testCase: TestCase = {
-  name: 'v4 agent request _iidt cookie',
+  name: 'v4 agent request no cookie',
   test: async (api) => {
     const { requestFromProxy } = await api.sendRequestToV4Cdn({
       request: {
         headers: { cookie: 'test=123; _iidt=test' },
       },
     })
-    assert(requestFromProxy.get('cookie'), '_iidt=test', 'cookie')
+    assertToBeFalsy('cookie', requestFromProxy.get('cookie'))
   },
 }
 
