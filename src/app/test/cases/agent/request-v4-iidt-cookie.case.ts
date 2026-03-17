ad00f886dd74b095d6c227e846e06eb4c612f064
Przemysław Żydek <przemyslawzydek@gmail.com>
2026-03-17 10:46:21 +0100

    feat: add and update test cases for `_iidt` cookie in v4 scenarios

--- src/app/test/cases/agent/request-v4-iidt-cookie.case.ts
+++ src/app/test/cases/agent/request-v4-iidt-cookie.case.ts
@@ -1,15 +1,15 @@
-import { assertToBeFalsy } from '../../service/assert'
+import { assert } from '../../service/assert'
 import { TestCase } from '../../types/testCase'
 
 const testCase: TestCase = {
-  name: 'v4 agent request no cookie',
+  name: 'v4 agent request _iidt cookie',
   test: async (api) => {
     const { requestFromProxy } = await api.sendRequestToV4Cdn({
       request: {
         headers: { cookie: 'test=123; _iidt=test' },
       },
     })
-    assertToBeFalsy('cookie', requestFromProxy.get('cookie'))
+    assert(requestFromProxy.get('cookie'), '_iidt=test', 'cookie')
   },
 }
 
