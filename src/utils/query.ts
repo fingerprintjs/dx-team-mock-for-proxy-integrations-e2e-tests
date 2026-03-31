858ca7c116c2e94c809233e466fcf73b993d9df6
Przemysław Żydek <przemyslawzydek@gmail.com>
2026-03-31 11:46:04 +0200

    fix: replace `Date.now` with `randomUUID` in `withCacheDisablingQueryParam` for improved cache bypass reliability

--- src/utils/query.ts
+++ src/utils/query.ts
@@ -1,5 +1,7 @@
+import { randomUUID } from 'crypto'
+
 export function withCacheDisablingQueryParam(query: URLSearchParams) {
   // Proxy integration should use `loaderVersion` and `version` as cache key,
   // Providing a random value will ensure that the request it not cached
-  query.set('loaderVersion', Date.now().toString())
+  query.set('loaderVersion', randomUUID())
 }
