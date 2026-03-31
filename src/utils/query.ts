9b91058b574ccd21de95a4d3732652763f738f99
Przemysław Żydek <przemyslawzydek@gmail.com>
2026-03-31 11:46:16 +0200

    fix: correct typo in `withCacheDisablingQueryParam` comment

--- src/utils/query.ts
+++ src/utils/query.ts
@@ -2,6 +2,6 @@ import { randomUUID } from 'crypto'
 
 export function withCacheDisablingQueryParam(query: URLSearchParams) {
   // Proxy integration should use `loaderVersion` and `version` as cache key,
-  // Providing a random value will ensure that the request it not cached
+  // Providing a random value will ensure that the request is not cached
   query.set('loaderVersion', randomUUID())
 }
